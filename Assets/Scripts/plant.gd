c46e00b7cd7d7e7661b848b3dae879cea0bdaee2
Lucas-kantola <48250445+LucasKantola@users.noreply.github.com>
2024-07-19 16:13:35 +0200

    Linjer som inte ville ansluta

--- Assets/Scripts/plant.gd
+++ Assets/Scripts/plant.gd
@@ -4,24 +4,22 @@ extends Node2D
 @onready var collider = $Area2D/CollisionShape2D
 @export var maxHeight = 100
 @export var growthRate = 1.1
+@export var defaultOffset := 10.0
 
 var currentHeight
+
+@export var branchScene: PackedScene = load("res://Assets/Scenes/Branch.tscn") 
 
 @export var platforms: Array = [
     {
         "position": Vector2(10, 20),
-        "size": Vector2(20, 10),
-        "texture": "res://Assets/Sprites/Sprite-0006.png",
         "rendered": false
     },
     {
         "position": Vector2(-10, 40),
-        "size": Vector2(20, 10),
-        "texture": "res://Assets/Sprites/Sprite-0006.png",
         "rendered": false
     }
 ]
-
 
 func _ready():
     currentHeight = sprite.get_rect().size.y
@@ -29,16 +27,33 @@ func _ready():
 func bodyEntered(body: Node2D) -> void:
     if body.is_in_group("Drop"):
         if currentHeight < maxHeight:
+            var lastHeight = currentHeight
             currentHeight *= growthRate
             sprite.scale.y *= growthRate
-            sprite.position.y -= currentHeight * (growthRate - 1)
+            sprite.position.y -= (currentHeight - lastHeight) / 2
 
         for platform in platforms:
             if currentHeight >= platform["position"].y:
                 if not platform["rendered"]:
                     platform["rendered"] = true
-                    var platformSprite = Sprite2D.new()
-                    platformSprite.texture = load(platform["texture"])
-                    platformSprite.position = platform["position"]
-                    platformSprite.scale = platform["size"]
-                    add_child(platformSprite)
+                    var platformScene = branchScene.instantiate()
+                    platformScene.position = Vector2(platform["position"].x, platform["position"].y * -1)
+                    add_child(platformScene)
+    queue_redraw()
+
+func _draw():
+    for platform in platforms:
+        if currentHeight >= platform["position"].y - defaultOffset:
+            var bottomPos = Vector2(0, -platform["position"].y + defaultOffset)
+            print("bottomPos: ", bottomPos)
+            var topPos = platform["position"]
+            
+            # Calculate the direction vector and the maximum distance the line can grow
+            var direction = bottomPos.direction_to(platform["position"])
+            var maxDistance = min(currentHeight, bottomPos.distance_to(platform["position"]))
+            
+            # Calculate the end point of the line based on the max distance
+            var endPos = bottomPos + direction * maxDistance
+            endPos.y *= -1
+            
+            draw_line(bottomPos, endPos, Color(1, 1, 1), 1)
